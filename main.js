45fb903be58de49b657dd9f0fac3d9f741355854
marcinhojazz <marcinhojazz@gmail.com>
2023-09-06 17:42:06 -0300

    update study project

--- main.js
+++ main.js
@@ -9,30 +9,63 @@ const createScene = function() {
   const scene = new BABYLON.Scene(engine);
 
   scene.createDefaultCameraOrLight(true, false, true);
-  const box = new BABYLON.MeshBuilder.CreateBox('myBox', {
-    size: 0.1,
-    width: 2,
-    height: 0.05,
-    depth: 0.5,
-    faceColors: [
-      new BABYLON.Color4(1, 0, 0, 1),
-      BABYLON.Color3.Green(),
-    ],
-    
-  });
+  scene.createDefaultCameraOrLight();
 
+  // const camera = new BABYLON.UniversalCamera('camera', new BABYLON.Vector3(0, 5, -10), scene);
+  const camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene)
+  camera.attachControl(true);
+  // camera.inputs.addMouseWheel();
+  // camera.setTarget(BABYLON.Vector3.Zero());
 
+  camera.setPosition(new BABYLON.Vector3(0, 0, -20));
+
+  camera.lowerBetaLimit = Math.PI / 4;
+  camera.upperBetaLimit = Math.PI / 2;
+
+  camera.lowerRadiusLimit = 20;
+  camera.upperRadiusLimit = 50;
+
+  // const box = new BABYLON.MeshBuilder.CreateBox('myBox', {
+  //   size: 0.1,
+  //   width: 2,
+  //   height: 0.05,
+  //   depth: 0.5,
+  //   faceColors: [
+  //     new BABYLON.Color4(1, 0, 0, 1),
+  //     BABYLON.Color3.Green(),
+  //   ],
+  // });
   
   const sphere = new BABYLON.MeshBuilder.CreateSphere('mySphere', {
     segments: 12,
     diameter: 0.3,
   });
 
-  const ground = new BABYLON.MeshBuilder.CreateGround('', {
-    height: 10,
-    width: 10,
+  const sphereMaterial = new BABYLON.StandardMaterial();
+  sphere.material = sphereMaterial;
 
-  })
+  sphereMaterial.diffuseColor = new BABYLON.Color3(0, 1, 0)
+  sphereMaterial.specularColor = new BABYLON.Color3(1, 0, 0)
+
+  // const ground = new BABYLON.MeshBuilder.CreateGround('', {
+  //   height: 10,
+  //   width: 10,
+  //   subdivisions: 30,
+  //   subdivisionX: 10
+  // })
+
+  // ground.material = new BABYLON.StandardMaterial();
+  // ground.material.wireframe = true; 
+  
+  // const groundFromHM = new BABYLON.MeshBuilder.CreateGroundFromHeightMap('', '/heightmap.png', {
+  //   height: 10,
+  //   width: 10,
+  //   subdivisions: 60,
+  //   maxHeight: 2
+  // });
+
+  // groundFromHM.material = new BABYLON.StandardMaterial();
+  // groundFromHM.material.wireframe = true; 
 
   return scene;
 }
